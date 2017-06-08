c2f93090d64f274d9ea39a213760f3ca912a66fa
spektremouse <tompachico@gmail.com>
2017-06-08 18:01:42 +0100

    Modified project structured.

--- Spikemoss/Spikemoss/Views/MainWindow.xaml.cs
+++ Spikemoss/Spikemoss/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Spikemoss
+namespace Spikemoss.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
