92f6fd2a3485c6b2926e6cd861a42bb744949b84
spektremouse <tompachico@gmail.com>
2017-06-08 17:14:18 +0100

    Added folder structure and operating system enum

---
+++ Spikemoss/Lime.Emerald/OperatingSystemType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lime.Emerald
+{
+    public enum OperatingSystemType
+    {
+        Windows,
+        Linux,
+        AIX,
+        Solaris,
+        HPUX
+    }
+}
